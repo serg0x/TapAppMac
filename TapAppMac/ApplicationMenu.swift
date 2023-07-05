d19408095d74b19de6047564e7dfe2e28c5b9214
Serg Lotz <blondewalker@gmail.com>
2023-07-05 15:14:39 +0200

    Re-init

---
+++ TapAppMac/ApplicationMenu.swift
@@ -0,0 +1,53 @@
+//
+//  ApplicationMenu.swift
+//  TapAppMac
+//
+//  Created by Sergej Lotz on 03.07.23.
+//
+
+import Foundation
+import SwiftUI
+
+class ApplicationMenu: NSObject {
+    let menu = NSMenu()
+    
+    func createMenu() -> NSMenu {
+        let contentView = ContentView()
+        let topView = NSHostingController(rootView: contentView)
+        topView.view.frame.size = CGSize(width: 225, height: 225)
+        
+        let customMenuItem = NSMenuItem()
+        customMenuItem.view = topView.view
+        menu.addItem(customMenuItem)
+        menu.addItem(NSMenuItem.separator())
+        
+        let aboutMenuItem = NSMenuItem(title: "About", action: #selector(about), keyEquivalent: "")
+        aboutMenuItem.target = self
+        menu.addItem(aboutMenuItem)
+        
+        let webLinkMenuItem = NSMenuItem(title: "Link to LMU", action: #selector(openLink), keyEquivalent: "")
+        webLinkMenuItem.target = self
+        webLinkMenuItem.representedObject = "https://www.medien.ifi.lmu.de/lehre/ss23/pml/"
+        menu.addItem(webLinkMenuItem)
+        
+        let quitMenuItem = NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q")
+        quitMenuItem.target = self
+        menu.addItem(quitMenuItem)
+        
+        return menu
+    }
+    
+    @objc func about(sender: NSMenuItem) {
+        NSApp.orderFrontStandardAboutPanel()
+    }
+    
+    @objc func openLink(sender: NSMenuItem) {
+        let link = sender.representedObject as! String
+        guard let url = URL(string: link) else { return }
+        NSWorkspace.shared.open(url)
+    }
+    
+    @objc func quit(sender: NSMenuItem) {
+        NSApp.terminate(self)
+    }
+}
