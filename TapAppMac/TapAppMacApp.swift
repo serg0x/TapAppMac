39675fc80b7c8417eeff8320bc06f55806504972
Serg Lotz <blondewalker@gmail.com>
2023-07-05 19:18:38 +0200

    Revert "Add one test implementation"
    This reverts commit 9d56a0b572da3b22b129c6bb771d6ff176564b4a.

--- TapAppMac/TapAppMacApp.swift
+++ TapAppMac/TapAppMacApp.swift
@@ -1,6 +1,11 @@
+//
+//  TapAppMacApp.swift
+//  TapAppMac
+//
+//  Created by Sergej Lotz on 02.07.23.
+//
+
 import SwiftUI
-import Cocoa
-import Network
 
 @main
 struct TapAppMacApp: App {
@@ -13,9 +18,6 @@ struct TapAppMacApp: App {
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var server: Server?
-    var listener: NWListener?
-    
     static private(set) var instance: AppDelegate!
     lazy var statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     let menu = ApplicationMenu()
@@ -24,53 +26,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         AppDelegate.instance = self
         statusBarItem.button?.title = "LMU PML"
         statusBarItem.menu = menu.createMenu()
-        
-        // Create and start the server
-        server = Server()
-        server?.startServer()
-        
-        // Enable Bonjour service discovery
-        startServiceDiscovery()
-    }
-
-    func startServiceDiscovery() {
-        listener = try? NWListener(using: .tcp, on: 0)
-        listener?.stateUpdateHandler = { [weak self] newState in
-            switch newState {
-            case .ready:
-                self?.publishService()
-            case .failed(let error):
-                print("Service discovery failed with error: \(error.localizedDescription)")
-            default:
-                break
-            }
-        }
-        listener?.start(queue: .main)
-    }
-    
-    func publishService() {
-        guard let port = listener?.port else {
-            print("Failed to get the listener port")
-            return
-        }
-        
-        let service = NetService(
-            domain: "",
-            type: "_myapp._tcp",
-            name: "MyServer",
-            port: 1337
-        )
-        service.delegate = self
-        service.publish()
-    }
-
-}
-extension AppDelegate: NetServiceDelegate {
-    func netServiceDidPublish(_ sender: NetService) {
-        print("Service published: \(sender.name)")
-    }
-    
-    func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
-        print("Failed to publish service: \(errorDict)")
     }
 }
